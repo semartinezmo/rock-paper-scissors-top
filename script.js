aa998230a5a44df8a310a6fea615e0a2048b38e8
Sebastian Martinez Moncada <86292263+semartinezmo@users.noreply.github.com>
2024-08-18 16:04:25 -0500

    Created index.html and script.js:
    1. Linked script.js to index.html
    2. Created first function that allows computer to pick between rock,
    paper, scissors using Math.random() if/else statements.

---
+++ script.js
@@ -0,0 +1,14 @@
+// Function to let the computer make a choice between rock, paper, or scissors
+
+function getComputerChoice() {
+  const randomNumber = Math.random();
+  if (randomNumber < 0.33) {
+    return "rock";
+  } else if (randomNumber < 0.66) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+console.log(getComputerChoice());
