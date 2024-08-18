a3e54146b97cf7f058055c47fd9d89086a3e7be0
Sebastian Martinez Moncada <86292263+semartinezmo@users.noreply.github.com>
2024-08-18 16:12:12 -0500

    Function to let user pick rock, paper, or scissors
    1. Created the second function to allow user to pick his choice
    2. Function allows to convert string to lower case if needed
    3. Function allows to check if the user choice is valid
    4. Function allow that if the user enters an invalid choice, alert them
    and call the function again

--- script.js
+++ script.js
@@ -12,3 +12,28 @@ function getComputerChoice() {
 }
 
 console.log(getComputerChoice());
+
+// Function to let user pick rock, paper, or scissors
+
+function getHumanChoice() {
+  // Prompt the user to enter a choice and convert it to lowercase if needed, also assign it to const humanChoice
+  const humanChoice = prompt(
+    "Please choose rock, paper, or scissors."
+  ).toLowerCase();
+  if (
+    humanChoice === "rock" ||
+    humanChoice === "paper" ||
+    humanChoice === "scissors"
+  ) {
+    return humanChoice;
+  } else {
+    // If the user enters an invalid choice, alert them and call the function again
+    alert(
+      humanChoice +
+        " is not a valid choice. Please choose rock, paper, or scissors."
+    );
+    return getHumanChoice();
+  }
+}
+
+console.log(getHumanChoice());
